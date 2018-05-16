29f27f7e0e4180af20587339d30eabac0dd24df3
atiunov <atiunov@unipro.ru>
2018-05-16 15:37:02 +0700

    UGENE-6067: 'installer' component added for installer selfupdating
    UGENE-6067: installer component with script for selfupdating
    
    UGENE-6067: script update for Linux x64
    
    UGENE-6067: installer component version fix; a typo fix; release date fix
    
    UGENE-6067: mac fix on installation
    
    UGENE-6067: page renaming fix and some debug info
    
    UGENE-6067: installer component script fix
    
    UGENE-6067: installer component script update

--- config/controller.js
+++ config/controller.js
@@ -1,18 +1,36 @@
-function Controller()
-{
+var installerUpdating = "InstallerUpdating";
+
+function isInstallerUpdatingInProgress() {
+    return installer.value(installerUpdating, false) == "true";
 }
 
-Controller.prototype.TargetDirectoryPageCallback = function()
-{
-    var widget = gui.currentPageWidget(); // get the current wizard page
+function setInstallerUpdatingInProgress(isInProgress) {
+    installer.setValue(installerUpdating, isInProgress);
+}
+
+function Controller() {
+
+}
+
+Controller.prototype.IntroductionPageCallback = function() {
+    if (isInstallerUpdatingInProgress()) {
+        console.log("Skip introduction page");
+        gui.clickButton(buttons.NextButton); // automatically click the Next button
+    }
+}
+
+Controller.prototype.TargetDirectoryPageCallback = function() {
+    var widget = gui.currentPageWidget();
     if (widget != null) {
         if (systemInfo.kernelType === "winnt") {
-            if ( systemInfo.currentCpuArchitecture === "i386") {
+            if (systemInfo.currentCpuArchitecture === "i386") {
                 var programFiles = installer.environmentVariable("ProgramFiles");
             }
-            if ( systemInfo.currentCpuArchitecture === "x86_64") {
+
+            if (systemInfo.currentCpuArchitecture === "x86_64") {
                 var programFiles = installer.environmentVariable("ProgramW6432");
             }
+
             if (programFiles != "") {
                 installer.setValue("TargetDir", programFiles + "\\Unipro UGENE");
                 widget.TargetDirectoryLineEdit.setText(programFiles + "\\Unipro UGENE");
